d718aab3daa37c49697d5acfc80d5ceec5560ca3
sebthian <sebastianr.palacios@gmail.com>
2018-01-12 00:32:38 -0500

    Plugin to translate DNA sequence

---
+++ limbo/plugins/translate.py
@@ -0,0 +1,55 @@
+"""!translate <DNA sequence> will return the protein sequence for <DNA sequence>"""
+
+import re
+try:
+    from urllib import quote
+except ImportError:
+    from urllib.request import quote
+import requests
+
+def translate(eq):
+
+    dnastring = quote(eq)
+    #dnastring = 'ATAATA'
+    table = {
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+        }
+    protein = []
+    end = len(dnastring) - (len(dnastring) %3) - 1
+    for i in range(0,end,3):
+        codon = dnastring[i:i+3]
+        if codon in table:
+            aminoacid = table[codon]
+            protein.append(aminoacid)
+        else:
+            protein.append("N")
+    return "".join(protein)
+    
+
+def on_message(msg, server):
+    text = msg.get("text", "")
+    #text = '!translate ATAATAATA'
+    match = re.findall(r"!translate (.*)", text)
+    
+    if not match:
+        return
+
+    return translate(match[0].encode("utf8"))
+    #return 'II'
+
+on_bot_message = on_message
